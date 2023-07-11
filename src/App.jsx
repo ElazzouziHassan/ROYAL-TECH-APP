83ea4c9d05bdad6dae5955577adb2a8cbecc64ed
El azzouzi Hassan <ezhassan.info@gmail.com>
2023-07-11 13:05:20 +0100

    main app setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Royal Tech | modern business solutions </h1>
+      <h2>coming soon ...</h2>
+    </div>
+  )
+}
+
+export default App
